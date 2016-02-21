fe53352fe410d3f8e7da70a6dbd4a0d8fad86e61
Brandon R. Stoner <monokrome@limpidtech.com>
2016-02-20 22:57:24 -0800

    Initial commit with super generic auth.

---
+++ horriblefilm/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include
+from django.conf.urls import url
+from django.contrib import admin
+
+
+urlpatterns = [
+    url(r'^account/', include('django.contrib.auth.urls')),
+    url(r'^account/', include('registration.backends.hmac.urls')),
+    url(r'^admin/', admin.site.urls),
+]
