f41951e4ac574893f92d5d0e15f2823b845086d5
Logangster <rockstar171@gmail.com>
2016-06-26 15:12:29 -0600

    Added home page

--- horriblefilm/urls.py
+++ horriblefilm/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
-
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.HomePage.as_view()),
     url(r'^account/', include('django.contrib.auth.urls')),
     url(r'^account/', include('registration.backends.hmac.urls')),
     url(r'^user/', include('users.urls')),
